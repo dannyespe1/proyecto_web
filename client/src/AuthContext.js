ebb20e4bffebefba4107c71f613603e8e77b7bab
Danny <djchicaiza5@espe.edu.ec>
2025-08-05 18:48:20 -0500

    mejora de la autenticación y detalles del producto

--- client/src/AuthContext.js
+++ client/src/AuthContext.js
@@ -1,19 +1,30 @@
+// src/AuthContext.jsx
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    // Cargar usuario al iniciar
-    useEffect(() => {
-        axios.get('/api/auth/me', { withCredentials: true })
-        .then(res => setUser(res.data))
-        .catch(() => setUser(null));
-    }, []);
-    return (
-        <AuthContext.Provider value={{ user, setUser }}>
-        {children}
-        </AuthContext.Provider>
-    );
+  const [user, setUser] = useState(null);
+
+  // Base URL de la API desde variable de entorno
+  const API = process.env.REACT_APP_API_URL;
+  // Ejemplo: "https://backend-d7qm.onrender.com"
+
+  // Cargar usuario al iniciar
+  useEffect(() => {
+    axios.get(
+      `${API}/api/auth/me`,
+      { withCredentials: true }
+    )
+    .then(res => setUser(res.data))
+    .catch(() => setUser(null));
+  }, [API]);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
+
