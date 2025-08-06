8be3536309078444da2d15eadb32f7968e95c917
Danny <djchicaiza5@espe.edu.ec>
2025-08-05 21:26:26 -0500

    correcion modals

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,41 @@
 // client/src/App.js
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import ProductList from './components/ProductList';
-import ProductDetail from './components/ProductDetail';
-import Cart from './components/Cart';
-import Home from './components/Home';
-import NavBar      from './components/NavBar';
-import Login       from './components/Login';
-import Register from './components/Register';
-import Checkout from './components/Checkout';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import NavBar          from './components/NavBar';
+import LoginModal      from './components/Login';      // Login.jsx exporta tu modal
+import RegisterModal   from './components/Register';   // Register.jsx exporta tu modal
+
+import Home            from './components/Home';
+import ProductList     from './components/ProductList';
+import ProductDetail   from './components/ProductDetail';
+import Cart            from './components/Cart';
+import Checkout        from './components/Checkout';
+import ProtectedRoute  from './components/ProtectedRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Siempre montados, fuera de <Routes> */}
       <NavBar />
-      <Login />
-      <Register />
+      <LoginModal />
+      <RegisterModal />
+
       <div className="container-flex text-center">
         <Routes>
-          <Route path="/"    element={<Home />} />
-          <Route path="/productos" element={<ProductList />} />
-          <Route path="/productos/:id" element={<ProductDetail />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login"        element={<Login />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout"element={<ProtectedRoute><Checkout /></ProtectedRoute>}/>
-          <Route path="*"     element={<h2>Página no encontrada</h2>} />
+          <Route path="/"                element={<Home />} />
+          <Route path="/productos"       element={<ProductList />} />
+          <Route path="/productos/:id"   element={<ProductDetail />} />
+          <Route path="/cart"            element={<Cart />} />
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*"                element={<h2>Página no encontrada</h2>} />
         </Routes>
       </div>
     </BrowserRouter>
